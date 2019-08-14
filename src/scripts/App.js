9f7d54895bc43dc64bb804fd09c9c998e49a18cb
HTMLhead <mrdoo309@gmail.com>
2019-08-14 23:41:54 +0900

    update(App): app추가

---
+++ src/scripts/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+// SPA main
+import PageRouter from "./Routers/index";
+
+function App() {
+  return (
+    <div className="App">
+      <PageRouter />
+    </div>
+  );
+}
+
+export default App;
