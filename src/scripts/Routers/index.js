4b0c34a4713ec41913fdf282c1e103349cc5562b
HTMLhead <mrdoo309@gmail.com>
2019-08-15 15:21:37 +0900

    update(router): exam라우팅 추가

--- src/scripts/Routers/index.js
+++ src/scripts/Routers/index.js
@@ -3,23 +3,34 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // Pages
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
+import LeftNav from "../Components/LeftNav";
 import NoMatch from "../Pages/NoMatch";
 import Main from "../Pages/Main";
+import Create from "../Pages/Create";
+import Content from "../Pages/Content";
+import Edit from "../Pages/Edit";
+import Exam from "../Pages/Exam";
+import ExamCardList from "../Pages/Exam/ExamCardList";
 
 function PageRouter() {
   return (
     <Router>
       <>
         <Route component={Header} />
-
-        <Switch>
-          {/* root Page */}
-          <Route exact path="/" component={Main} />
-
-          {/* 404 page */}
-          <Route component={NoMatch} />
-        </Switch>
-
+        <div style={{ display: "flex" }}>
+          <Route component={LeftNav} />
+          <Switch>
+            {/* root Page */}
+            <Route exact path="/" component={Main} />
+            <Route exact path="/create" component={Create} />
+            <Route exact path="/edit/:id" component={Edit} />
+            <Route exact path="/exam" component={Exam} />
+            <Route exact path="/exam/:class" component={ExamCardList} />
+            <Route path="/:class" component={Content} />
+            {/* 404 page */}
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
         <Route component={Footer} />
       </>
     </Router>
