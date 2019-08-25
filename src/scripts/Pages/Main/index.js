08b90ee43998bdd4b9bc1f101e1e64fe29f57fe6
HTMLhead <mrdoo309@gmail.com>
2019-08-25 19:29:56 +0900

    update(main): main페이지 theme적용

--- src/scripts/Pages/Main/index.js
+++ src/scripts/Pages/Main/index.js
@@ -1,31 +1,36 @@
 import React from "react";
 // css-in-js
-import { makeStyles } from "@material-ui/styles";
+import { makeStyles, useTheme } from "@material-ui/styles";
 // custom-component
 
 const useStyles = makeStyles({
-  root: {
+  root: ({ jettBlack }) => ({
     width: "100%",
     height: "100vh",
     display: "flex",
-    backgroundColor: "#000",
-    color: "red",
+    backgroundColor: jettBlack[300],
+    color: jettBlack[100],
     justifyContent: "center",
     flexDirection: "column",
-  },
+  }),
   text: {
     textAlign: "center",
-    fontWiehgt: "Bold",
     fontSize: "3rem",
     marginBottom: ".8rem",
+  },
+  notification: {
+    textAlign: "left",
+    fontWeight: "Bold",
   },
 });
 
 function Main() {
-  const classes = useStyles();
+  const { palette } = useTheme();
+  const classes = useStyles({ jettBlack: palette.jettBlack });
+
   return (
     <div className={classes.root}>
-      <span className={classes.text}>이 글은 공유가 금지되어 있습니다.</span>
+      <span className={classes.text}>공지사항</span>
       <span className={classes.text}>이 글은 어떠한 책임도 져주지 않습니다.</span>
       <span className={classes.text}>이 글은 위험성을 가지고 있습니다.</span>
       <span className={classes.text}>이 글을 공유시 끔찍한 일이 벌어질 수 있습니다.</span>
