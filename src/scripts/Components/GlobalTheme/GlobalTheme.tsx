3a008a67484d8f13ed243358239c53e6d4b020c0
HTMLhead <mrdoo309@gmail.com>
2019-09-13 22:20:30 +0900

    update(globalTheme): 클로벌 테마 타입스크립트 적용

---
+++ src/scripts/Components/GlobalTheme/GlobalTheme.tsx
@@ -0,0 +1,13 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const GlobalTheme = createMuiTheme({
+  jettBlack: {
+    100: "#45433e",
+    200: "#66635c",
+    300: "#99958b",
+    400: "#ccc6b9",
+    500: "#fff8e7",
+  },
+});
+
+export default GlobalTheme;
