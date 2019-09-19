9e90fe79f5603d0cd88145568796fe344606d29d
HTMLhead <mrdoo309@gmail.com>
2019-09-19 21:24:36 +0900

    update(footer): footer 타입스크립트로 전환

--- src/scripts/Components/Footer/index.tsx
+++ src/scripts/Components/Footer/index.tsx
@@ -1,27 +1,27 @@
 import React from "react";
 // css-in-js
-import { makeStyles, useTheme } from "@material-ui/styles";
+// import { makeStyles, useTheme } from "@material-ui/styles";
+import { makeStyles, Theme } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 
-const useStyles = makeStyles({
-  root: ({ jettBlack }) => ({
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
     fontFamily: "'Apple SD Gothic Neo', sans-serif",
-    backgroundColor: jettBlack[100],
+    backgroundColor: theme.jettBlack[100],
     height: "4rem",
-  }),
-  copyright: ({ jettBlack }) => ({
-    color: jettBlack[500],
-  }),
+  },
+  copyright: {
+    color: theme.jettBlack[500],
+  },
   gridRoot: {
     height: "100%",
   },
-});
+}));
 
 const footerMsg = `무단 배포를 엄금합니다.`;
 
 function Footer() {
-  const { jettBlack } = useTheme();
-  const classes = useStyles({ jettBlack });
+  const classes = useStyles();
 
   return (
     <footer className={classes.root} id="footer">
