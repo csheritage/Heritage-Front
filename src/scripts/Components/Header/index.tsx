e85ce85666fe7030b95c5e9dcf8bbb34a982cecd
HTMLhead <mrdoo309@gmail.com>
2019-09-19 21:24:59 +0900

    update(header): 헤더 tsx로 전환, theme변경

--- src/scripts/Components/Header/index.tsx
+++ src/scripts/Components/Header/index.tsx
@@ -1,22 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 // css-in-js
-import { makeStyles, useTheme } from "@material-ui/styles";
+import { makeStyles, Theme } from "@material-ui/core";
 import { AppBar, Toolbar } from "@material-ui/core";
 
-const useStyles = makeStyles({
-  root: ({ jettBlack }) => ({
-    backgroundColor: jettBlack[100],
-    color: jettBlack[500],
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    backgroundColor: theme.jettBlack[100],
+    color: theme.jettBlack[500],
     height: "4rem",
     justifyContent: "space-between",
     padding: "0 8rem",
-  }),
-});
+  },
+}));
 
 function Header() {
-  const { jettBlack } = useTheme();
-  const classes = useStyles({ jettBlack });
+  const classes = useStyles();
 
   return (
     <AppBar position="static" color="inherit" elevation={0}>
