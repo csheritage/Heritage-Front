a2cc5ed6f3036055c87026e54017a32dd06f341f
HTMLhead <mrdoo309@gmail.com>
2019-09-26 19:50:52 +0900

    Updates

---
+++ precache-manifest.3040974e61b5bfaaf3dbeababd92c520.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a17c05f1626f5539a1cad88c29c80f31",
+    "url": "/Heritage-Front/index.html"
+  },
+  {
+    "revision": "0a133b7a58563f6ab9a3",
+    "url": "/Heritage-Front/static/css/main.a2531c7a.chunk.css"
+  },
+  {
+    "revision": "032ecdae85ad92b33662",
+    "url": "/Heritage-Front/static/js/2.6ed9b4f0.chunk.js"
+  },
+  {
+    "revision": "0a133b7a58563f6ab9a3",
+    "url": "/Heritage-Front/static/js/main.f05e1b5c.chunk.js"
+  },
+  {
+    "revision": "343b2ec6912a60cccddd",
+    "url": "/Heritage-Front/static/js/runtime~main.aec4f492.js"
+  }
+]);
